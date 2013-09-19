af451bdd06b5a78884541ff4caed6c92fcb6cb9f
Victor Pantoja <victor.pantoja@gmail.com>
2013-09-19 20:37:10 -0300

    Improving setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='python-jenkins',
       version='0.2',
       description='Python bindings for the remote Jenkins API',
+      long_description='Python bindings for the remote Jenkins API',
       author='Ken Conley',
       author_email='kwc@willowgarage.com',
       url='http://launchpad.net/python-jenkins',
-      packages=['jenkins'],
+      packages=find_packages(),
+      package_dir={"jenkins": "jenkins"},
+      include_package_data=True,
      )
